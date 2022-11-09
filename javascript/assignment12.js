caace1a4fd63db66a820d9023c4125ca102e0784
enguy3n <etng4880@colorado.edu>
2022-11-09 03:43:42 -0700

    unfinished files for assignment 12

---
+++ javascript/assignment12.js
@@ -0,0 +1,50 @@
+// text content vars
+const areaCode = document.querySelector('.areaCode');
+const midDigits = document.querySelector('.firstThree');
+const lastDigits = document.querySelector('.finalDigits');
+const verification = document.querySelector('.verification');
+
+// input selectors
+const areaCodeSelector = document.querySelector('.areaCodeButton');
+const submitSelector = document.querySelector('.submitButton');
+
+areaCodeSelector.addEventListener('click', modifyAreaCode);
+submitSelector.addEventListener('click', submit);
+
+function reset(){
+    const defaultVal = 0;
+    areaCode.textContent = defaultVal.toString().padStart(3, '0');
+    midDigits.textContent = defaultVal.toString().padStart(3, '0');
+    lastDigits.textContent = defaultVal.toString().padStart(4, '0');
+
+    //dummy values to text reset selectors
+    // areaCode.textContent = 123;
+    // midDigits.textContent = 123;
+    // lastDigits.textContent = 1234;
+}
+
+function modifyAreaCode(){
+    console.log('modifying area code');
+
+    let min = 0;
+    let max = 3000;
+    let newValue = Math.floor(Math.random() * (max - min) + min);
+    console.log('area code: got value: ', newValue);
+
+    if(newValue >= 201 && newValue <= 999){
+        areaCode.textContent = newValue;
+    }else{
+        reset();
+        alert('area code ' + newValue + ' is not a valid area code.');
+    }
+}
+
+function submit(){
+    console.log('submit pressed');
+
+    let stringContent = 'your phone number has been recieved. your number was ';
+    stringContent += '(' + areaCode.textContent
+                   + ')' + midDigits.textContent
+                   + '-' + lastDigits.textContent + '.';
+    verification.textContent = stringContent;
+}
