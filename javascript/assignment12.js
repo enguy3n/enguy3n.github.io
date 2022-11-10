3ecadf9b91aaf9ab2c4997f012289090d604ce38
enguy3n <etng4880@colorado.edu>
2022-11-10 16:52:20 -0700

    finished, probably

--- javascript/assignment12.js
+++ javascript/assignment12.js
@@ -10,10 +10,21 @@ const midDigitsSelector = document.querySelector('.middleDigitsButton');
 const lastDigitsSelector = document.querySelector('.lastDigitsButton');
 const submitSelector = document.querySelector('.submitButton');
 
+// pass box
+const passBox = document.querySelector('.passWrapper');
+const key = document.querySelector('.passPhrase');
+const generateLastDigits = document.querySelector('.generateLastDigits');
+const feedback = document.querySelector('.feedback');
+const userInput = document.getElementById('passInput');
+const submitPass = document.querySelector('.submitPass');
+
 areaCodeSelector.addEventListener('click', modifyAreaCode);
 midDigitsSelector.addEventListener('click', modifyMid);
-lastDigitsSelector.addEventListener('click', modifyLast);
+lastDigitsSelector.addEventListener('click', callFinal);
 submitSelector.addEventListener('click', submit);
+submitPass.addEventListener('click', checkPhrase);
+generateLastDigits.addEventListener('click', generateFinal);
+
 
 // resets number back to (000)000-0000
 function reset(){
@@ -28,9 +39,11 @@ function reset(){
     // lastDigits.textContent = 1234;
 }
 
-// causes submission text to disapepar when any button is pressed
-function resetSubmission(){
-    verification.textContent = '';
+// resets passbox
+function resetPassBox(){
+    passBox.style.display = 'none';
+    generateLastDigits.style.display = 'none';
+    feedback.textContent = '';
 }
 
 function modifyAreaCode(){
@@ -48,6 +61,7 @@ function modifyAreaCode(){
     }else{
         reset();
         alert('area code ' + newValue + ' is not a valid area code.  your number has been reset.');
+        resetPassBox();
     }
 }
 
@@ -67,15 +81,85 @@ function modifyMid(){
     }else{
         reset();
         alert('digits ' + newValue + ' cannot fit here.  your number has been reset.');
+        resetPassBox();
     }
 }
 
-function modifyLast(){
+function generateFinal(){
+    console.log('generating final');
+
+    resetSubmission();
+
+    let min = 0;
+    let max = 20000;
+
+    let newValue = Math.floor(Math.random() * (max - min) + min);
+    console.log('final: got value: ', newValue);
+
+    if(newValue >= 0 && newValue < 10000){
+        lastDigits.textContent = newValue.toString().padStart(4, '0');
+    }else{
+        reset();
+        alert('digits ' + newValue + ' cannot fit here.  your number has been reset.');
+        resetPassBox();
+    }
+}
+
+// pass box stuff
+function callFinal(){
     console.log('modifying last digits');
 
     resetSubmission();
+    openPassBox();
 }
 
+function openPassBox(){
+    console.log('pass box triggered');
+    passBox.style.display = 'block';
+
+    generatePassPhrase();
+}
+
+function generatePassPhrase(){
+    console.log('generating passphrase');
+    let passPhrase = '';
+    for(let i = 0; i < 25; i++){
+        // choose whether this value is a number or letter
+        let charType = Math.floor(Math.random() * (10-0) + 0);
+
+        if(charType % 2 === 0){
+            // number
+            let newValue = Math.floor(Math.random() * (58 - 48) + 48);
+            newValue = String.fromCharCode(newValue);
+            passPhrase += newValue;
+        }else{
+            // letter
+            let newValue = Math.floor(Math.random() * (123 - 97) + 97);
+            console.log(newValue);
+            newValue = String.fromCharCode(newValue);
+            passPhrase += newValue;
+        }
+        console.log(passPhrase);
+    }
+
+    key.textContent = passPhrase;
+}
+
+function checkPhrase(){
+    inputPhrase = userInput.value;
+    console.log(inputPhrase);
+
+    if(inputPhrase === key.textContent){
+        feedback.textContent = '';
+        generateLastDigits.style.display = 'block';
+    }else{
+        feedback.textContent = 'incorrect phrase.  please try again.'
+        generatePassPhrase();
+        generateLastDigits.style.display = 'none';
+    }
+}
+
+// final submission
 function submit(){
     console.log('submit pressed');
 
@@ -86,4 +170,10 @@ function submit(){
     verification.textContent = stringContent;
 
     reset();
+    resetPassBox();
+}
+
+// causes submission text to disapepar when any button is pressed
+function resetSubmission(){
+    verification.textContent = '';
 }
