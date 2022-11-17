0d4dd849b4dd3eabbad4050ba571f36f2eb6c699
enguy3n <etng4880@colorado.edu>
2022-11-17 16:12:11 -0700

    added callback, fixed error on element selection

--- javascript/ica13.js
+++ javascript/ica13.js
@@ -13,7 +13,34 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
   
+//   alice1.animate(aliceTumbling, aliceTiming);
+
+  // callback implimentation
+  function recurse(target, callback){
+    const promiseVal = target.animate(aliceTumbling, aliceTiming).finished;
+    console.log(promiseVal);
+    callback(promiseVal);
+  }
+
+  function callBack(){
+    recurse(alice1, (result1)=>{
+        recurse(alice2, (result2)=>{
+            recurse(alice3, (result3)=>
+            {
+                console.log('something happened');
+            });
+        });
+    });
+  }
+
+  // promises implementation
+  function promises(){
   alice1.animate(aliceTumbling, aliceTiming).finished
     .then(()=>alice2.animate(aliceTumbling, aliceTiming).finished)
-    .then(()=>alice2.animate(aliceTumbling, aliceTiming).finished)
+    .then(()=>alice3.animate(aliceTumbling, aliceTiming).finished)
     .catch((error)=>console.error('error: ${error}'));
+  }
+
+//   callBack();
+
+  promises();
