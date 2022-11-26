ab62583b10d6fc8a728c8b2cb229d434a15bbfde
enguy3n <etng4880@colorado.edu>
2022-11-26 05:15:53 -0700

    very basic grid

--- game/game.js
+++ game/game.js
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/34896106/attach-event-to-dynamic-elements-in-javascript
+// https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+
 const height = 4;
 const width = 4;
 console.log('this is running');
@@ -8,7 +11,7 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
+    // cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
